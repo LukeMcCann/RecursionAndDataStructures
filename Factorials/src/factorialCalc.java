b6901700f9a1a5e04c2d38d6f7fbf4e01205270a
L.J.McCann U1364096 <U1364096@unimail.hud.ac.uk>
2020-02-13 11:15:32 +0000

    Adds a recursive factorial calculator

---
+++ Factorials/src/factorialCalc.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class factorialCalc {
+
+    private static BufferedReader In = new BufferedReader( new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a number (0 to quit): ");
+        int number = Integer.parseInt(In.readLine());
+
+        while(number != 0) {
+            System.out.println("The factorial of: " + number + " is: ");
+            System.out.println(recursiveFactorial(number));
+
+            System.out.print("Enter a number (0 to quit): ");
+            number = Integer.parseInt(In.readLine());
+        }
+    }
+
+    private static int recursiveFactorial(int N) {
+        if(N == 1) {
+            return N;
+        } else {
+            return N * recursiveFactorial(N-1);
+        }
+    }
+
+}
